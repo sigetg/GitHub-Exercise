e35ed8456f458a5ec78dfdb2b447d3ddb2b7b11e
sigetg <sigetyg@bc.edu>
2023-01-20 11:56:51 -0500

    Version 4.0 - Purple

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "3.circle")
+            Image(systemName: "4.circle")
                 .resizable()
                 .scaledToFit()
-            Text("V. 3.0")
+            Text("V. 4.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
             Rectangle()
         }
         .padding()
-        .foregroundColor(.blue)
+        .foregroundColor(.purple)
     }
 }
 
