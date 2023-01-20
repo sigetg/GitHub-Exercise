1ea7b7ce46a3300554fa906090ce4b7f3f1d36b0
sigetg <sigetyg@bc.edu>
2023-01-20 11:51:53 -0500

    Version 3.0 - Blue

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,15 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "2.circle")
+            Image(systemName: "3.circle")
                 .resizable()
                 .scaledToFit()
-            Text("V. 2.0")
+            Text("V. 3.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
         .padding()
-        .foregroundColor(.green)
+        .foregroundColor(.blue)
     }
 }
 
