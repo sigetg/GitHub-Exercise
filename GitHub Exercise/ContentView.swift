1f9a9da96568c8bc72f82b9d9ac766335ab180af
sigetg <sigetyg@bc.edu>
2023-01-20 11:47:00 -0500

    development - added Rectangle()

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("V. 2.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
+            Rectangle()
         }
         .padding()
         .foregroundColor(.green)
